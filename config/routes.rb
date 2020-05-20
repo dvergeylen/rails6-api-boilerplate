3c3f1215e61b9c4956dc962fe7a321e13ec73cc1
Daniel Vergeylen <daniel.vergeylen@sigmadelta.io>
2020-05-20 14:12:14 +0200

    feat: enhanced routing to fallback to latest API Version

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,9 @@ Rails.application.routes.draw do
 #     namespace 'v2' do
 #       concerns :api_base
 #     end
+
+    match 'v:api/*path', to: redirect('/api/v1/%{path}'), via: :all
+    match '/*path',      to: redirect('/api/v1/%{path}'), via: :all
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
